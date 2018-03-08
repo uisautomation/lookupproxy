2aba81758a68571eb8fc525447eb80b1471e2d92
Rich Wareham <rjw57@cam.ac.uk>
2018-03-08 11:38:55 +0000

    default to test lookup except in production
    To enable easy testing of group memberships in development, default to
    the test lookup instance in all but the production docker image where we
    default to real lookup.

--- lookupproxy/settings/docker.py
+++ lookupproxy/settings/docker.py
@@ -8,3 +8,6 @@ STATIC_ROOT = '/usr/src/app/build/static'
 MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ] + MIDDLEWARE  # noqa: F405
+
+# In production, use the production lookup
+LOOKUP_API_ENDPOINT_HOST = 'www.lookup.cam.ac.uk'
